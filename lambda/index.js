9fde921a5e1a3fa2f62ae16515cfa5018784ae6f
sugandhsahu07 <sugandh.sahu@informa.com>
2024-05-26 21:25:08 +0530

    feat: lambda code

---
+++ lambda/index.js
@@ -0,0 +1,96 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+var AWS = require('aws-sdk');
+exports.handler = function (event) { return __awaiter(_this, void 0, void 0, function () {
+    var s3, bucket, key, data, s3Items_1, params, videoData, err_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                console.log('S3 EVENT:', JSON.stringify(event));
+                s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+                bucket = 'video-storage-poc-tricon';
+                key = 'video-data.json';
+                _a.label = 1;
+            case 1:
+                _a.trys.push([1, 4, , 5]);
+                data = [];
+                s3Items_1 = {};
+                params = {
+                    Bucket: bucket,
+                    Prefix: '', // Empty prefix to list objects at the root level
+                };
+                return [4 /*yield*/, s3.listObjectsV2(params).promise()];
+            case 2:
+                videoData = _a.sent();
+                videoData = videoData.Contents.map(function (obj) { return obj.Key; });
+                console.log("Video:", videoData);
+                videoData.forEach(function (key) {
+                    var value = key.split('/');
+                    if (value.length === 3) {
+                        if (!s3Items_1[value[1]])
+                            s3Items_1[value[1]] = { name: value[1] };
+                        if (value[2].includes('.jpg')) {
+                            s3Items_1[value[1]]['posterImage'] = value[2];
+                        }
+                        else {
+                            s3Items_1[value[1]]['key'] = value[2];
+                        }
+                    }
+                });
+                console.log('s3items', s3Items_1);
+                data = Object.values(s3Items_1);
+                // Store data in destination S3 object
+                return [4 /*yield*/, s3.putObject({ Body: JSON.stringify(data), Bucket: bucket, Key: key }).promise()];
+            case 3:
+                // Store data in destination S3 object
+                _a.sent();
+                return [2 /*return*/, {
+                        statusCode: 200,
+                        body: JSON.stringify("Data transferred from ".concat(bucket, "/").concat(key, " to ").concat(bucket, "/").concat(key)),
+                    }];
+            case 4:
+                err_1 = _a.sent();
+                console.error(err_1);
+                return [2 /*return*/, {
+                        statusCode: 500,
+                        body: JSON.stringify("Error: ".concat(err_1.message)),
+                    }];
+            case 5: return [2 /*return*/];
+        }
+    });
+}); };
