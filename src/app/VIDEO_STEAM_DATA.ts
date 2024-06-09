02a8fe8237b81fab7aed359caf7bfe958e508899
Nidhi Kumari <nidhi.kumari@Triconinfotech.com>
2024-06-09 15:38:04 +0530

    feat: side bar modified

---
+++ src/app/VIDEO_STEAM_DATA.ts
@@ -0,0 +1,174 @@
+export const VIDEO_STEAM_DATA = [
+    {
+        "key": "Agarwal_Advanced",
+        "path": "",
+        "items": [
+            {
+                "key": "SECTION_I_OCULOPLASTY_AND_ORBIT",
+                "path": "",
+                "items": [
+                    {
+                        "key": "Asian_Blepharoplasty/",
+                        "path": "",
+                        "items": []
+                    },
+                    {
+                        "key": "Blepharoplasty_with_Fibrin_Glue/",
+                        "path": "",
+                        "items": []
+                    }
+                ]
+            },
+            {
+                "key": "SECTION_II_CONJUNCTIVA_AND_CORNEA",
+                "path": "",
+                "items": [
+                    {
+                        "key": "Artificial_Corneal_Transplant/",
+                        "path": "",
+                        "items": []
+                    },
+                    {
+                        "key": "Big_Bubble_Technique_DALK/",
+                        "path": "",
+                        "items": []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "key": "Agarwal_Basic",
+        "path": "",
+        "items": [
+            {
+                "key": "SECTION_I_OCULOPLASTY_AND_ORBIT",
+                "path": "",
+                "items": [
+                    {
+                        "key": "Asian_Blepharoplasty/",
+                        "path": "",
+                        "items": []
+                    },
+                    {
+                        "key": "Blepharoplasty_with_Fibrin_Glue/",
+                        "path": "",
+                        "items": []
+                    }
+                ]
+            },
+            {
+                "key": "SECTION_II_CONJUNCTIVA_AND_CORNEA",
+                "path": "",
+                "items": [
+                    {
+                        "key": "Artificial_Corneal_Transplant/",
+                        "path": "",
+                        "items": []
+                    },
+                    {
+                        "key": "Big_Bubble_Technique_DALK/",
+                        "path": "",
+                        "items": []
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+export const VIDEO_STEAM_DATA_OLD = [
+    {
+        "Agarwal_Advanced": {
+            "path": "",
+            "items": [
+                {
+                    "SECTION_I_OCULOPLASTY_AND_ORBIT": {
+                        "path": "",
+                        "items": [
+                            {
+                                "Asian_Blepharoplasty/": {
+                                    "path": "",// "Agarwal_Advanced/SECTION_I_OCULOPLASTY_AND_ORBIT/Asian_Blepharoplasty/media.mp4"
+                                    "items": [
+
+                                    ]
+                                }
+                            },
+                            {
+                                "Blepharoplasty_with_Fibrin_Glue/": {
+                                    "path": "",
+                                    "items": [
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    "SECTION_II_CONJUNCTIVA_AND_CORNEA": {
+                        "path": "",
+                        "items": [
+                            {
+                                "Artificial_Corneal_Transplant/": {
+                                    "path": "",
+                                    "items": []
+                                }
+                            },
+                            {
+                                "Big_Bubble_Technique_DALK/": {
+                                    "path": "",
+                                    "items": []
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "Agarwal_Basic": {
+            "path": "",
+            "items": [
+                {
+                    "SECTION_I_OCULOPLASTY_AND_ORBIT": {
+                        "path": "",
+                        "items": [
+                            {
+                                "Asian_Blepharoplasty/": {
+                                    "path": "",
+                                    "items": []
+                                }
+                            },
+                            {
+                                "Blepharoplasty_with_Fibrin_Glue/": {
+                                    "path": "",
+                                    "items": []
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    "SECTION_II_CONJUNCTIVA_AND_CORNEA": {
+                        "path": "",
+                        "items": [
+                            {
+                                "Artificial_Corneal_Transplant/": {
+                                    "path": "",
+                                    "items": []
+                                }
+                            },
+                            {
+                                "Big_Bubble_Technique_DALK/": {
+                                    "path": "",
+                                    "items": []
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
+]
---
+++ src/app/video-stream-list/video-stream-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { VIDEO_STEAM_DATA } from '../VIDEO_STEAM_DATA';
+import { VideostreamService } from '../videostream.service';
+
+interface VideoItem {
+  key: string;
+  path: string;
+  items: VideoItem[];
+}
+
+@Component({
+  selector: 'app-video-stream-list',
+  templateUrl: './video-stream-list.component.html',
+  styleUrls: ['./video-stream-list.component.scss']
+})
+export class VideoStreamListComponent implements OnInit {
+  VIDEO_STREAM_DATA: VideoItem[] = VIDEO_STEAM_DATA;
+  expandedKey: string | null = null;
+
+  constructor(private videostreamService: VideostreamService) { }
+
+  ngOnInit(): void {
+    console.log('VIDEO_STREAM_DATA::', this.VIDEO_STREAM_DATA);
+  }
+
+  saveVideoStream(dataSet: VideoItem | VideoItem[], key:string): void {
+    console.log('saveVideoStream method with::', dataSet, ' key:',key);
+    this.videostreamService.saveCurrVideoItemArr(dataSet);
+    this.toggleExpand(key);
+  }
+
+  hasSubItems(obj: VideoItem): boolean {
+    console.log('hasSubItems::',obj);
+    return obj.items && obj.items.length > 0;
+  }
+
+  toggleExpand(key: string): void {
+    if (this.expandedKey === key) {
+      this.expandedKey = null;
+    } else {
+      this.expandedKey = key;
+    }
+  }
+}
